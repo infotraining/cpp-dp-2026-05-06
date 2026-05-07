9e2e3832c247a70cf544a8136b253db9e8e05c9a
Krystian Piekos <krystian.piekos@infotraining.pl>
2026-05-07 13:49:43 +0000

    Composite for Shapes

--- Structural/Composite.Exercise/shape_group.hpp
+++ Structural/Composite.Exercise/shape_group.hpp
@@ -10,8 +10,84 @@ namespace Drawing
 {
     // TODO: implement a composite for shapes
     // TODO: implement Iterator pattern for ShapeGroup composite that allows iterating over aggregated shapes
-    class ShapeGroup
+    class ShapeGroup : public CloneableShape<ShapeGroup>
     {
+        std::vector<std::unique_ptr<Shape>> shapes_;
+    public:
+        static constexpr auto id = "ShapeGroup";
+
+        using iterator = typename std::vector<std::unique_ptr<Shape>>::iterator;
+        using const_iterator = typename std::vector<std::unique_ptr<Shape>>::const_iterator;
+
+        ShapeGroup() = default;
+
+        ShapeGroup(const ShapeGroup& source)
+        {
+            for(const auto& shp : source.shapes_)
+            {
+                shapes_.push_back(shp->clone());
+            }
+        }
+
+        ShapeGroup& operator=(const ShapeGroup& source)
+        {
+            ShapeGroup temp{source}; // cc
+            swap(temp);
+
+            return *this;
+        }
+
+        ShapeGroup(ShapeGroup&&) = default;
+        ShapeGroup& operator=(ShapeGroup&&) = default;
+
+        void swap(ShapeGroup& other)
+        {
+            shapes_.swap(other.shapes_);
+        }
+
+        void add_shape(std::unique_ptr<Shape> shp)
+        {
+            shapes_.push_back(std::move(shp));
+        }
+
+        void draw() const override
+        {
+            for(const auto& shp : shapes_)
+                shp->draw();
+        }
+
+        void move(int dx, int dy) override
+        {
+            for(auto& shp : shapes_)
+            {
+                shp->move(dx, dy);
+            }
+        }
+
+        size_t size() const
+        {
+            return shapes_.size();
+        }
+
+        iterator begin()
+        {
+            return shapes_.begin();
+        }
+
+        iterator end()
+        {
+            return shapes_.end();
+        }
+
+        const_iterator begin() const
+        {
+            return shapes_.begin();
+        }
+
+        const_iterator end() const
+        {
+            return shapes_.end();
+        }
     };
 }
 
