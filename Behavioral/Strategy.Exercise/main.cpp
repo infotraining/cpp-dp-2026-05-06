337843a916e1a879471fa25d84c19c37343ec8c4
Krystian Piekos <krystian.piekos@infotraining.pl>
2026-05-08 10:17:07 +0000

    Strategy pattern implementation with modern C++ features, including std::function and lambda expressions. The code defines a set of formatting strategies for transforming strings, and a data context that uses these strategies to pretty-print data. Additionally, the exercise demonstrates the use of the Strategy pattern for statistical calculations, allowing for flexible addition of new statistics without modifying existing code.

--- Behavioral/Strategy.Exercise/main.cpp
+++ Behavioral/Strategy.Exercise/main.cpp
@@ -7,15 +7,16 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <functional>
+#include <cmath>
 
 struct StatResult
 {
     std::string description;
     double value;
 
-    StatResult(const std::string& desc, double val)
-        : description(desc)
-        , value(val)
+    StatResult(const std::string &desc, double val)
+        : description(desc), value(val)
     {
     }
 };
@@ -23,26 +24,78 @@ struct StatResult
 using Data = std::vector<double>;
 using Results = std::vector<StatResult>;
 
-enum StatisticsType
+// enum StatisticsType
+// {
+//     avg,
+//     min_max,
+//     sum
+// };
+
+using Statistics = std::function<void(const Data &, Results &)>;
+
+void avg(const Data &data, Results &results)
 {
-    avg,
-    min_max,
-    sum
-};
+    double sum = std::accumulate(data.begin(), data.end(), 0.0);
+    double avg = sum / data.size();
+    results.push_back(StatResult("Avg", avg));
+}
+
+void min(const Data &data, Results &results)
+{
+    double min = *(std::min_element(data.begin(), data.end()));
+    results.push_back(StatResult("Min", min));
+}
+
+void max(const Data &data, Results &results)
+{
+    double max = *(std::max_element(data.begin(), data.end()));
+    results.push_back(StatResult("Max", max));
+}
+
+// class MinMax
+// {
+// public:
+//     void operator()(const Data &data, Results &results) const
+//     {
+//         double min = *(std::min_element(data.begin(), data.end()));
+//         double max = *(std::max_element(data.begin(), data.end()));
+
+//         results.push_back(StatResult("Min", min));
+//         results.push_back(StatResult("Max", max));
+//     }
+// };
+
+void sum(const Data &data, Results &results)
+{
+    double sum = std::accumulate(data.begin(), data.end(), 0.0);
+    results.push_back(StatResult("Sum", sum));
+}
+
+void stddev(const Data &data, Results &results)
+{
+    double sum = std::accumulate(data.begin(), data.end(), 0.0);
+    double avg = sum / data.size();
+
+    double stdev = std::sqrt(std::accumulate(data.begin(), data.end(), 0.0, [avg](double acc, double d)
+                                             { return acc + (d - avg) * (d - avg); }) /
+                             data.size());
+
+    results.push_back(StatResult("Standard Deviation", stdev));
+}
 
 class DataAnalyzer
 {
-    StatisticsType stat_type_;
+    Statistics stat_strategy_;
     Data data_;
     Results results_;
 
 public:
-    DataAnalyzer(StatisticsType stat_type)
-        : stat_type_{stat_type}
+    DataAnalyzer(Statistics stat_strategy)
+        : stat_strategy_{stat_strategy}
     {
     }
 
-    void load_data(const std::string& file_name)
+    void load_data(const std::string &file_name)
     {
         data_.clear();
         results_.clear();
@@ -60,70 +113,75 @@ public:
         std::cout << "File " << file_name << " has been loaded...\n";
     }
 
-    void set_statistics(StatisticsType stat_type)
+    void set_statistics(Statistics stat_strategy)
     {
-        stat_type_ = stat_type;
+        stat_strategy_ = stat_strategy;
     }
 
     void calculate()
     {
-        if (stat_type_ == avg)
-        {
-            double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
-            double avg = sum / data_.size();
-
-            StatResult result("Avg", avg);
-            results_.push_back(result);
-        }
-        else if (stat_type_ == min_max)
-        {
-            double min = *(std::min_element(data_.begin(), data_.end()));
-            double max = *(std::max_element(data_.begin(), data_.end()));
-
-            results_.push_back(StatResult("Min", min));
-            results_.push_back(StatResult("Max", max));
-        }
-        else if (stat_type_ == sum)
-        {
-            double sum = std::accumulate(data_.begin(), data_.end(), 0.0);
-
-            results_.push_back(StatResult("Sum", sum));
-        }
+        stat_strategy_(data_, results_);
     }
 
-    const Results& results() const
+    const Results &results() const
     {
         return results_;
     }
 };
 
-void show_results(const Results& results)
+void show_results(const Results &results)
 {
-    for (const auto& rslt : results)
+    for (const auto &rslt : results)
         std::cout << rslt.description << " = " << rslt.value << std::endl;
 }
+
+class StatGroup
+{
+    std::vector<Statistics> stats_;
+
+public:
+    void add(Statistics stat)
+    {
+        stats_.push_back(stat);
+    }
+
+    void operator()(const Data &data, Results &results) const
+    {
+        for (const auto &strategy : stats_)
+        {
+            strategy(data, results);
+        }
+    }
+};
 
 int main()
 {
     // TODO: Refactor the code above using Strategy Pattern
     // Hint#1: Define an interface for calculating Statistics
-    // Hint#2: Use delegation technique to replace ifs in calculate() method 
+    // Hint#2: Use delegation technique to replace ifs in calculate() method
 
-    DataAnalyzer da{avg};
+    StatGroup min_max;
+    min_max.add(min);
+    min_max.add(max);
+
+    StatGroup common_stats;
+    common_stats.add(avg);
+    common_stats.add(min_max);
+    common_stats.add(sum);
+
+    StatGroup adv_stats;
+    adv_stats.add(common_stats);
+    adv_stats.add(stddev);
+
+    DataAnalyzer da{common_stats};
     da.load_data("stats_data.dat");
-
-    da.calculate();
-
-    da.set_statistics(min_max);
-    da.calculate();
-
-    da.set_statistics(sum);
     da.calculate();
 
     show_results(da.results());
 
     std::cout << "\n\n";
 
+    da.set_statistics(adv_stats);
     da.load_data("new_stats_data.dat");
     da.calculate();
 
